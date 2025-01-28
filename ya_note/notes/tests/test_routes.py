7bcf1844f0d3187b401133c0c009ed6d22cb2517
Kirill Turochkin <kirill180104@mail.ru>
2025-01-28 13:31:32 +0500

    new test test_pages_availability_user + docstring

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -8,10 +8,11 @@ from notes.models import Note
 
 User = get_user_model()
 
+
 class TestRoutes(TestCase):
 
     @classmethod
-    def setUpTestData(cls):
+    def setUpTestData(cls) -> None:
         cls.author = User.objects.create(username='Лев Толстой')
         cls.another_author = User.objects.create(username='warfolomey')
         cls.note = Note.objects.create(
@@ -21,9 +22,23 @@ class TestRoutes(TestCase):
             author=cls.author,
         )
 
-    def test_pages_availability_anonymus(self):
-        '''Тест доступности страниц анонимных пользователей'''
-        urls = (
+    def test_pages_availability_user(self) -> None:
+        """Тест на доступность пользователя на list, done, add"""
+        self.client.force_login(self.author)
+        urls: tuple = (
+            ('notes:list', None),
+            ('notes:success', None),
+            ('notes:add', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url: str = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_pages_availability_anonymus(self) -> None:
+        """Тест доступности страниц анонимных пользователей"""
+        urls: tuple = (
             ('notes:home', None),
             ('users:login', None),
             ('users:logout', None),
@@ -31,31 +46,33 @@ class TestRoutes(TestCase):
         )
         for name, args in urls:
             with self.subTest(name=name):
-                url = reverse(name, args=args)
+                url: str = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_redirect_for_anonymous_client_change_note(self):
-        login_url = reverse('users:login')
+    def test_redirect_for_anonymous_client_change_note(self) -> None:
+        """Перенаправления анонимного пользователя на вход с заметки"""
+        login_url: str = reverse('users:login')
         for name in ('notes:delete', 'notes:detail', 'notes:edit'):
             with self.subTest(name=name):
-                # print(name)
-                url = reverse(name, args=(self.note.slug,))
-                redirect_url = f'{login_url}?next={url}'
+                url: str = reverse(name, args=(self.note.slug,))
+                redirect_url: str = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
 
-    def test_redirect_for_anonymous_client(self):
-        login_url = reverse('users:login')
+    def test_redirect_for_anonymous_client(self) -> None:
+        """Перенаправления анонимного пользователя"""
+        login_url: str = reverse('users:login')
         for name in ('notes:list', 'notes:success', 'notes:add'):
             with self.subTest(name=name):
-                url = reverse(name)
-                redirect_url = f'{login_url}?next={url}'
+                url: str = reverse(name)
+                redirect_url: str = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
 
-    def test_availability_for_note_edit_and_delete(self):
-        users_statuses = (
+    def test_availability_for_note_edit_and_delete(self) -> None:
+        """Тест на доступность редактирования и удаления заметки"""
+        users_statuses: tuple = (
             (self.author, HTTPStatus.OK),
             (self.another_author, HTTPStatus.NOT_FOUND),
         )
@@ -63,6 +80,6 @@ class TestRoutes(TestCase):
             self.client.force_login(user)
             for name in ('notes:edit', 'notes:delete', 'notes:detail'):
                 with self.subTest(user=user, name=name):
-                    url = reverse(name, args=(self.note.slug,))
+                    url: str = reverse(name, args=(self.note.slug,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
