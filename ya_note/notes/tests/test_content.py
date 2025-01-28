43acec97510e6ef51fc277af8610c964cb2239d6
Kirill Turochkin <kirill180104@mail.ru>
2025-01-28 13:30:15 +0500

    added annotation and new test and docstring

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -11,10 +11,10 @@ User = get_user_model()
 
 class TestListNotePage(TestCase):
 
-    HOME_URL = reverse('notes:list')
+    HOME_URL: str = reverse('notes:list')
 
     @classmethod
-    def setUpTestData(cls):
+    def setUpTestData(cls) -> None:
         cls.author = User.objects.create(username='warfolomey')
         all_notes = [
             Note(
@@ -27,36 +27,53 @@ class TestListNotePage(TestCase):
         ]
         Note.objects.bulk_create(all_notes)
 
-    def test_notes_order(self):
+    def test_notes_order(self) -> None:
+        """Тест на отображения заметок по возрастанию"""
         self.client.force_login(self.author)
         response = self.client.get(self.HOME_URL)
         object_list = response.context['object_list']
-        all_id = [notes.id for notes in object_list]
-        sorted_id = sorted(all_id)
+        all_id: list = [notes.id for notes in object_list]
+        sorted_id: list = sorted(all_id)
         self.assertEqual(all_id, sorted_id)
 
 
 class TestDetailPage(TestCase):
 
     @classmethod
-    def setUpTestData(cls):
+    def setUpTestData(cls) -> None:
         cls.author = User.objects.create(username='warfolomey')
+        cls.another_author = User.objects.create(username='Danil')
         cls.note = Note.objects.create(
             title='заметка',
             text='просто текст',
             slug='hello',
             author=cls.author,
         )
+        cls.note_another = Note.objects.create(
+            title='Это замека Danil',
+            text='просто текст',
+            slug='danil',
+            author=cls.another_author,
+        )
+        cls.list_url = reverse('notes:list')
         cls.add_url = reverse('notes:add')
         cls.update_url = reverse('notes:edit', args=(cls.note.slug,))
 
-    def test_authorized_client_has_form_create(self):
+    def test_list_author_note(self) -> None:
+        """Тест на получение только заметок автора"""
+        self.client.force_login(self.author)
+        response = self.client.get(self.list_url)
+        self.assertNotIn(self.note_another, response.context['object_list'])
+
+    def test_authorized_client_has_form_create(self) -> None:
+        """Тест на отображения формы создания заметки"""
         self.client.force_login(self.author)
         response = self.client.get(self.add_url)
         self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
 
-    def test_authorized_client_has_form_update(self):
+    def test_authorized_client_has_form_update(self) -> None:
+        """Тест на отображение формы на редактирования заметки"""
         self.client.force_login(self.author)
         response = self.client.get(self.update_url)
         self.assertIn('form', response.context)
