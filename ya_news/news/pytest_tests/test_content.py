bd551a4a5cbffd477b2e2631a0792f9bc7cc8e7b
Kirill Turochkin <kirill180104@mail.ru>
2025-01-29 15:29:38 +0500

    reforming import, corrected test

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,45 +1,43 @@
 import pytest
-
 from django.conf import settings
-from django.urls import reverse
 
 from news.forms import CommentForm
 
 
-@pytest.mark.django_db()
+pytestmark = pytest.mark.django_db
+
+
 def test_news_count(
     ten_news,
+    url_reverse_home,
     client,
 ) -> None:
-    """Тест на корректность количества отображаемых новостей"""
-    url: str = reverse('news:home')
-    response = client.get(url)
+    """Тест на корректность количества отображаемых новостей."""
+    response = client.get(url_reverse_home)
     object_list = response.context['object_list']
     news_count: int = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-@pytest.mark.django_db()
 def test_news_order(
     ten_news,
+    url_reverse_home,
     client,
 ) -> None:
-    """Тест на корректность порядка отображения новостей"""
-    url: str = reverse('news:home')
-    response = client.get(url)
-    object_list = response.context['object_list']
-    all_dates: list = [news.date for news in object_list]
+    """Тест на корректность порядка отображения новостей."""
+    response = client.get(url_reverse_home)
+    all_dates: list = [news.date for news in response.context['object_list']]
     sorted_dates: list = sorted(all_dates, reverse=True)
     assert all_dates == sorted_dates
 
 
 def test_comments_order(
     author_client,
+    url_reverse_detail,
     many_comments,
 ) -> None:
-    """Тест на корректность порядка отображения комментариев"""
-    url: str = reverse('news:detail', args=(many_comments.pk, ))
-    response = author_client.get(url)
+    """Тест на корректность порядка отображения комментариев."""
+    response = author_client.get(url_reverse_detail)
     assert 'news' in response.context
     news = response.context['news']
     all_comments = news.comment_set.all()
@@ -48,23 +46,20 @@ def test_comments_order(
     assert all_timestamps == sorted_timestamps
 
 
-@pytest.mark.django_db()
 def test_anonymous_client_has_no_form(
     client,
-    pk_new_for_args: tuple,
+    url_reverse_detail,
 ) -> None:
-    """Тест на отображение формы у анонимных пользователей"""
-    url: str = reverse('news:detail', args=pk_new_for_args)
-    response = client.get(url)
+    """Тест на отображение формы у анонимных пользователей."""
+    response = client.get(url_reverse_detail)
     assert 'form' not in response.context
 
 
 def test_authorized_client_has_form(
     author_client,
-    pk_new_for_args: tuple,
+    url_reverse_detail,
 ) -> None:
-    """Тест на отображение формы у залогиненых пользователей"""
-    url: str = reverse('news:detail', args=pk_new_for_args)
-    response = author_client.get(url)
+    """Тест на отображение формы у залогиненых пользователей."""
+    response = author_client.get(url_reverse_detail)
     assert 'form' in response.context
     assert isinstance(response.context['form'], CommentForm)
