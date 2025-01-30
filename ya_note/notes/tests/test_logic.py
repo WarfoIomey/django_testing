3cb6d7bdcddcc16ed26ebd765f352e22971efce8
Kirill Turochkin <kirill180104@mail.ru>
2025-01-30 11:58:51 +0500

    transfer setUpTestData for fixture.py, corrected test

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,47 +1,11 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase
-from django.contrib.auth import get_user_model, get_user
-from django.urls import reverse
+from django.contrib.auth import get_user
 from pytils.translit import slugify
 
 from notes.models import Note
 from notes.forms import WARNING
-
-
-User = get_user_model()
-
-
-class FixtureSetUpTestData(TestCase):
-    ONE_NOTE: int = 1
-
-    @classmethod
-    def setUpTestData(cls) -> None:
-        cls.author = User.objects.create(username='warfolomey')
-        cls.another_user = User.objects.create(username='Danil')
-        cls.author_client = Client()
-        cls.author_client.force_login(cls.author)
-        cls.another_user_client = Client()
-        cls.another_user_client.force_login(cls.another_user)
-        cls.note = Note.objects.create(
-            title='Test',
-            text='Text',
-            slug='hello',
-            author=cls.author,
-        )
-        cls.form_data = {
-            'title': 'Test',
-            'text': 'Просто текст',
-            'slug': 'test',
-        }
-        cls.url_add = reverse('notes:add')
-        cls.url_done = reverse('notes:success')
-        cls.url_edit = reverse('notes:edit', args=(cls.note.slug,))
-        cls.url_delete = reverse('notes:delete', args=(cls.note.slug,))
-
-    def get_note(self, id_note):
-        """Метод получения заметки по id."""
-        return Note.objects.get(id=id_note)
+from .fixture import FixtureSetUpTestData
 
 
 class TestNoteCreation(FixtureSetUpTestData):
@@ -50,8 +14,8 @@ class TestNoteCreation(FixtureSetUpTestData):
         """Тест на незаполненный slug."""
         Note.objects.all().delete()
         self.form_data.pop('slug')
-        response = self.author_client.post(self.url_add, data=self.form_data)
-        self.assertRedirects(response, self.url_done)
+        response = self.client_author.post(self.add_url, data=self.form_data)
+        self.assertRedirects(response, self.done_url)
         assert Note.objects.count() == self.ONE_NOTE
         new_note = Note.objects.get()
         expected_slug = slugify(self.form_data['title'])
@@ -60,22 +24,22 @@ class TestNoteCreation(FixtureSetUpTestData):
     def test_anonymous_user_cant_create_note(self) -> None:
         """Тест на создания заметки анонимным пользователем."""
         later_count_notes: int = Note.objects.count()
-        self.client.post(self.url_add, data=self.form_data)
+        self.client.post(self.add_url, data=self.form_data)
         notes_count: int = Note.objects.count()
         self.assertEqual(notes_count, later_count_notes)
 
     def test_user_can_create_note(self) -> None:
         """Тест на создания заметки залогиненым пользователем."""
         Note.objects.all().delete()
-        response = self.author_client.post(self.url_add, data=self.form_data)
-        self.assertRedirects(response, self.url_done)
+        response = self.client_author.post(self.add_url, data=self.form_data)
+        self.assertRedirects(response, self.done_url)
         notes_count: int = Note.objects.count()
         self.assertEqual(notes_count, self.ONE_NOTE)
         note = Note.objects.get()
         self.assertEqual(note.title, self.form_data['title'])
         self.assertEqual(note.text, self.form_data['text'])
         self.assertEqual(note.slug, self.form_data['slug'])
-        self.assertEqual(note.author, get_user(self.author_client))
+        self.assertEqual(note.author, get_user(self.client_author))
 
 
 class TestNoteEditDelete(FixtureSetUpTestData):
@@ -83,32 +47,36 @@ class TestNoteEditDelete(FixtureSetUpTestData):
     def test_author_can_delete_note(self) -> None:
         """Тест на удаление автором заметки."""
         later_count_notes: int = Note.objects.count()
-        response = self.author_client.delete(self.url_delete)
-        self.assertRedirects(response, self.url_done)
+        response = self.client_author.delete(self.delete_url)
+        self.assertRedirects(response, self.done_url)
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, later_count_notes - self.ONE_NOTE)
 
     def test_user_cant_delete_note_of_another_user(self) -> None:
         """Тест на удаление заметки другим пользователем."""
-        response = self.another_user_client.delete(self.url_delete)
+        later_notes_count: int = Note.objects.count()
+        response = self.another_author_client.delete(self.delete_url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         notes_count: int = Note.objects.count()
-        self.assertEqual(notes_count, self.ONE_NOTE)
+        self.assertEqual(notes_count, later_notes_count)
 
     def test_author_can_edit_note(self) -> None:
         """Тест на редактирования автором заметки."""
-        response = self.author_client.post(self.url_edit, data=self.form_data)
-        self.assertRedirects(response, self.url_done)
+        response = self.client_author.post(
+            self.update_url,
+            data=self.form_data
+        )
+        self.assertRedirects(response, self.done_url)
         new_note = self.get_note(self.note.id)
         self.assertEqual(new_note.title, self.form_data['title'])
         self.assertEqual(new_note.text, self.form_data['text'])
         self.assertEqual(new_note.slug, self.form_data['slug'])
-        self.assertEqual(new_note.author, get_user(self.author_client))
+        self.assertEqual(new_note.author, self.note.author)
 
     def test_not_unique_slug(self) -> None:
         """Тест на уникальность slug."""
         self.form_data['slug'] = self.note.slug
-        response = self.author_client.post(self.url_add, data=self.form_data)
+        response = self.client_author.post(self.add_url, data=self.form_data)
         self.assertFormError(
             response,
             'form',
@@ -118,8 +86,8 @@ class TestNoteEditDelete(FixtureSetUpTestData):
 
     def test_user_cant_edit_note_of_another_user(self) -> None:
         """Тест на редактирования заметки другого автора."""
-        response = self.another_user_client.post(
-            self.url_edit,
+        response = self.another_author_client.post(
+            self.update_url,
             data=self.form_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
