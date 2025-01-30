6148ed3e5d3107b05c80cafab260079d65c3cebd
Kirill Turochkin <kirill180104@mail.ru>
2025-01-30 11:58:33 +0500

    transfer setUpTestData for fixture.py

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,41 +1,4 @@
-from django.test import Client, TestCase
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-
-from notes.models import Note
-from notes.forms import NoteForm
-
-
-User = get_user_model()
-
-
-class FixtureSetUpTestData(TestCase):
-
-    @classmethod
-    def setUpTestData(cls) -> None:
-        cls.author = User.objects.create(username='warfolomey')
-        cls.client_author = Client()
-        cls.client_author.force_login(cls.author)
-        cls.another_author = User.objects.create(username='Danil')
-        cls.note = Note.objects.create(
-            title='заметка топ',
-            text='просто текст',
-            slug='hello',
-            author=cls.author,
-        )
-        cls.note_another = Note.objects.create(
-            title='Это замека Danil',
-            text='просто текст',
-            slug='danil',
-            author=cls.another_author,
-        )
-        cls.list_url = reverse('notes:list')
-        cls.add_url = reverse('notes:add')
-        cls.update_url = reverse('notes:edit', args=(cls.note.slug,))
-        cls.urls_forms = (
-            (cls.add_url, NoteForm),
-            (cls.update_url, NoteForm),
-        )
+from .fixture import FixtureSetUpTestData
 
 
 class TestDetailPage(FixtureSetUpTestData):
@@ -43,8 +6,8 @@ class TestDetailPage(FixtureSetUpTestData):
     def test_separate_note(self) -> None:
         """Тест на передачу отдельной заметки в context."""
         response = self.client_author.get(self.list_url)
-        object_list = response.context['object_list']
-        self.assertIn(self.note, object_list)
+        note = response.context['object_list']
+        self.assertIn(self.note, note)
 
     def test_list_author_note(self) -> None:
         """Тест на получение только заметок автора."""
@@ -54,7 +17,7 @@ class TestDetailPage(FixtureSetUpTestData):
     def test_authorized_client_has_form_create_update(self) -> None:
         """Тест на отображения формы создания и редактирования заметки."""
         for url, form in self.urls_forms:
-            with self.subTest():
+            with self.subTest(url=url, form=form):
                 response = self.client_author.get(url)
                 self.assertIn('form', response.context)
                 self.assertIsInstance(response.context['form'], form)
